6618582f22ab2e61885b2f15f01c1d9491c7fab5
Brian Friesen <brianfriesen@quickenloans.com>
2017-07-20 11:03:50 -0400

    Add xml doc comments

--- AppSettings.cs
+++ AppSettings.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 
 namespace RockLib.Configuration
 {
+    /// <summary>
+    /// Defines an indexer property that retrieves settings from the "AppSettings" section from
+    /// <see cref="ConfigurationManager.ConfigurationRoot"/>.
+    /// </summary>
     public sealed class AppSettings
     {
         private AppSettings() { }
 
         internal static AppSettings Instance { get; } = new AppSettings();
 
+        /// <summary>
+        /// Gets the setting associated with the <paramref name="key"/> parameter.
+        /// </summary>
+        /// <param name="key">The key of the setting to look up.</param>
+        /// <returns>The setting associated with the <paramref name="key"/> parameter.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// If the given key is not found in the "AppSettings" section of <see cref="ConfigurationManager.ConfigurationRoot"/>.
+        /// </exception>
         public string this[string key] => ConfigurationManager.ConfigurationRoot["AppSettings:" + key] ?? throw GetKeyNotFoundExeption(key);
 
         private static Exception GetKeyNotFoundExeption(string key) =>
--- ConfigurationManager.cs
+++ ConfigurationManager.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace RockLib.Configuration
 {
+    /// <summary>
+    /// Defines a central location to store an instance of <see cref="IConfigurationRoot"/>.
+    /// </summary>
     public static class ConfigurationManager
     {
         private static readonly object _locker = new object();
@@ -15,12 +18,32 @@ namespace RockLib.Configuration
             ResetConfigurationRoot();
         }
 
+        /// <summary>
+        /// Gets an object that retrieves settings from the "AppSettings" section of the
+        /// <see cref="ConfigurationRoot"/> property.
+        /// </summary>
         public static AppSettings AppSettings => AppSettings.Instance;
 
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="ConfigurationRoot"/> property is the default
+        /// instance of <see cref="IConfigurationRoot"/>.
+        /// </summary>
         public static bool IsDefault { get; private set; } = true;
 
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="ConfigurationRoot"/> property has been locked.
+        /// <para>Tha value of this property is <c>false</c> before the <see cref="ConfigurationRoot"/> property
+        /// has been accessed and true after it has been accessed. When this property is true, any calls to the
+        /// <see cref="SetConfigurationRoot(IConfigurationRoot)"/>,
+        /// <see cref="SetConfigurationRoot(Func{IConfigurationRoot})"/>, or <see cref="ResetConfigurationRoot"/>
+        /// methods will result in an <see cref="InvalidOperationException"/>.</para>
+        /// </summary>
         public static bool IsLocked => _configurationRoot != null;
 
+        /// <summary>
+        /// Gets the <see cref="IConfigurationRoot"/> associated with the <see cref="ConfigurationManager"/> class.
+        /// This property is guaranteed not to change.
+        /// </summary>
         public static IConfigurationRoot ConfigurationRoot
         {
             get
@@ -42,18 +65,50 @@ namespace RockLib.Configuration
             }
         }
 
+        /// <summary>
+        /// Sets the value of the <see cref="ConfigurationRoot"/> property to the specified
+        /// <see cref="IConfigurationRoot"/> instance.
+        /// <para>NOTE: This method should only be called at the beginning of an application. Any calls to this method after
+        /// the <see cref="ConfigurationRoot"/> property has been accessed (i.e. when <see cref="IsLocked"/> is true) will
+        /// result in an <see cref="InvalidOperationException"/> being thrown.</para>
+        /// </summary>
+        /// <param name="configurationRoot">
+        /// The instance of <see cref="IConfigurationRoot"/> to be used as the <see cref="ConfigurationRoot"/> property.
+        /// </param>
+        /// <exception cref="ArgumentNullException">If the <paramref name="configurationRoot"/> parameter is null.</exception>
+        /// <exception cref="InvalidOperationException">If the <see cref="IsLocked"/> property is true.</exception>
         public static void SetConfigurationRoot(IConfigurationRoot configurationRoot)
         {
             if (configurationRoot == null) throw new ArgumentNullException(nameof(configurationRoot));
             SetConfigurationRoot(() => configurationRoot);
         }
 
+        /// <summary>
+        /// Sets the value of the <see cref="ConfigurationRoot"/> property to the instance returned by the specified
+        /// callback function. The callback function MUST NOT return null.
+        /// <para>NOTE: This method should only be called at the beginning of an application. Any calls to this method after
+        /// the <see cref="ConfigurationRoot"/> property has been accessed (i.e. when <see cref="IsLocked"/> is true) will
+        /// result in an <see cref="InvalidOperationException"/> being thrown.</para>
+        /// </summary>
+        /// <param name="getConfigurationRoot">
+        /// A function that returns the instance of <see cref="IConfigurationRoot"/> to be used as the <see cref="ConfigurationRoot"/>
+        /// property. This function MUST NOT return null.
+        /// </param>
+        /// <exception cref="ArgumentNullException">If the <paramref name="getConfigurationRoot"/> parameter is null.</exception>
+        /// <exception cref="InvalidOperationException">If the <see cref="IsLocked"/> property is true.</exception>
         public static void SetConfigurationRoot(Func<IConfigurationRoot> getConfigurationRoot)
         {
             if (getConfigurationRoot == null) throw new ArgumentNullException(nameof(getConfigurationRoot));
             SetConfigurationRoot(getConfigurationRoot, false);
         }
 
+        /// <summary>
+        /// Resets the value of the <see cref="ConfigurationRoot"/> property to the default instance.
+        /// <para>NOTE: This method should only be called at the beginning of an application. Any calls to this method after
+        /// the <see cref="ConfigurationRoot"/> property has been accessed (i.e. when <see cref="IsLocked"/> is true) will
+        /// result in an <see cref="InvalidOperationException"/> being thrown.</para>
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If the <see cref="IsLocked"/> property is true.</exception>
         public static void ResetConfigurationRoot()
         {
             SetConfigurationRoot(GetDefaultConfigurationRoot, true);
