70690eb70116f81bea65cd47a264b9fd8b9b39af
Brian Friesen <brianfriesen@quickenloans.com>
2017-11-01 16:09:56 -0400

    Add ValueConverters and tests

---
+++ RockLib.Configuration.ObjectFactory.UnitTests/ValueConvertersTests.cs
@@ -0,0 +1,302 @@
+﻿using RockLib.Configuration.ObjectFactory;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class ValueConvertersTests
+    {
+        [Fact]
+        public void CanAddConvertFuncByDeclaringTypeAndMemberName()
+        {
+            // Register a converter by declaring type and member name when you need
+            // different properties of the same type to use different converters.
+
+            // Use this Add method when you want to register a function that returns
+            // the converted value.
+
+            // Call the Add method for each converter that needs to be registered.
+            var valueConverters = new ValueConverters();
+            valueConverters.Add(typeof(Foo), "bar", value => new Bar(int.Parse(value)));
+            valueConverters.Add(typeof(Foo), "baz", ParseBaz);
+
+            // The Add method returns 'this', so you can chain them together:
+            valueConverters = new ValueConverters()
+                .Add(typeof(Foo), "bar", value => new Bar(int.Parse(value)))
+                .Add(typeof(Foo), "baz", ParseBaz);
+
+            // List initialization syntax works also.
+            valueConverters = new ValueConverters
+            {
+                { typeof(Foo), "bar", value => new Bar(int.Parse(value)) },
+                { typeof(Foo), "baz", ParseBaz }
+            };
+        }
+
+        [Fact]
+        public void CanAddConverterTypeByDeclaringTypeAndMemberName()
+        {
+            // Register a converter by declaring type and member name when you need
+            // different properties of the same type to use different converters.
+
+            // Use this Add method when you want to register a type that declares
+            // a 'static T Convert(string)' method.
+
+            // Call the Add method for each converter that needs to be registered.
+            var valueConverters = new ValueConverters();
+            valueConverters.Add(typeof(Foo), "bar", typeof(BarConverter));
+            valueConverters.Add(typeof(Foo), "baz", typeof(BazConverter));
+
+            // The Add method returns 'this', so you can chain them together:
+            valueConverters = new ValueConverters()
+                .Add(typeof(Foo), "bar", typeof(BarConverter))
+                .Add(typeof(Foo), "baz", typeof(BazConverter));
+
+            // List initialization syntax works also.
+            valueConverters = new ValueConverters
+            {
+                { typeof(Foo), "bar", typeof(BarConverter) },
+                { typeof(Foo), "baz", typeof(BazConverter) }
+            };
+        }
+
+        [Fact]
+        public void CanAddConvertFuncByTargetType()
+        {
+            // Register a converter by target type when you want all properties of the
+            // target type to use the same converter.
+
+            // Use this Add method when you want to register a function that returns
+            // the converted value.
+
+            // Call the Add method for each converter that needs to be registered.
+            var valueConverters = new ValueConverters();
+            valueConverters.Add(typeof(Bar), value => new Bar(int.Parse(value)));
+            valueConverters.Add(typeof(Baz), ParseBaz);
+
+            // The Add method returns 'this', so you can chain them together:
+            valueConverters = new ValueConverters()
+                .Add(typeof(Bar), value => new Bar(int.Parse(value)))
+                .Add(typeof(Baz), ParseBaz);
+
+            // List initialization syntax works also.
+            valueConverters = new ValueConverters
+            {
+                { typeof(Bar), value => new Bar(int.Parse(value)) },
+                { typeof(Baz), ParseBaz }
+            };
+        }
+
+        [Fact]
+        public void CanAddConverterTypeByTargetType()
+        {
+            // Register a converter by target type when you want all properties of the
+            // target type to use the same converter.
+
+            // Use this Add method when you want to register a type that declares
+            // a 'static T Convert(string)' method.
+
+            // Call the Add method for each converter that needs to be registered.
+            var valueConverters = new ValueConverters();
+            valueConverters.Add(typeof(Bar), typeof(BarConverter));
+            valueConverters.Add(typeof(Baz), typeof(BazConverter));
+
+            // The Add method returns 'this', so you can chain them together:
+            valueConverters = new ValueConverters()
+                .Add(typeof(Bar), typeof(BarConverter))
+                .Add(typeof(Baz), typeof(BazConverter));
+
+            // List initialization syntax works also.
+            valueConverters = new ValueConverters
+            {
+                { typeof(Bar), typeof(BarConverter) },
+                { typeof(Baz), typeof(BazConverter) }
+            };
+        }
+
+        [Fact]
+        public void TryGetByDeclaringTypeAndMemberNameReturnsTrueWhenThereIsAMatch()
+        {
+            var valueConverters = new ValueConverters()
+                .Add(typeof(Foo), "bar", typeof(BarConverter))
+                .Add(typeof(Foo), "baz", ParseBaz);
+
+            Assert.True(valueConverters.TryGet(typeof(Foo), "bar", out var convertBar));
+            Assert.True(valueConverters.TryGet(typeof(Foo), "baz", out var convertBaz));
+        }
+
+        [Fact]
+        public void TryGetByDeclaringTypeAndMemberNameReturnsFalseWhenThereNotIsAMatch()
+        {
+            var valueConverters = new ValueConverters();
+
+            Assert.False(valueConverters.TryGet(typeof(Foo), "bar", out var convertBar));
+            Assert.False(valueConverters.TryGet(typeof(Foo), "baz", out var convertBaz));
+        }
+
+        [Fact]
+        public void TryGetByTargetTypeReturnsTrueWhenThereIsAMatch()
+        {
+            var valueConverters = new ValueConverters()
+                .Add(typeof(Bar), value => new Bar(int.Parse(value)))
+                .Add(typeof(Baz), ParseBaz);
+
+            Assert.True(valueConverters.TryGet(typeof(Bar), out var convertBar));
+            Assert.True(valueConverters.TryGet(typeof(Baz), out var convertBaz));
+        }
+
+        [Fact]
+        public void TryGetByTargetTypeReturnsFalseWhenThereNotIsAMatch()
+        {
+            var valueConverters = new ValueConverters();
+
+            Assert.False(valueConverters.TryGet(typeof(Bar), out var convertBar));
+            Assert.False(valueConverters.TryGet(typeof(Baz), out var convertBaz));
+        }
+
+        [Fact]
+        public void TheConvertFuncReturnedByTryGetCallsTheConvertFuncPassedToTheAddByDeclaringTypeAndMemberNameMethod()
+        {
+            var invocationCount = 0;
+            Func<string, Bar> registeredConvertBar = value =>
+            {
+                invocationCount++;
+                return new Bar(int.Parse(value));
+            };
+
+            var valueConverters = new ValueConverters()
+                .Add(typeof(Foo), "bar", registeredConvertBar);
+
+            Assert.True(valueConverters.TryGet(typeof(Foo), "bar", out var retrievedConvertBar));
+
+            Assert.Equal(0, invocationCount);
+
+            var converted = retrievedConvertBar("123");
+
+            Assert.Equal(1, invocationCount);
+            Assert.IsType<Bar>(converted);
+            Assert.Equal(123, ((Bar)converted).Baz);
+        }
+
+        [Fact]
+        public void TheConvertFuncReturnedByTryGetCallsTheConvertFuncPassedToTheAddByTargetTypeMethod()
+        {
+            var invocationCount = 0;
+            Func<string, Baz> registeredConvertBaz = value =>
+            {
+                invocationCount++;
+                return ParseBaz(value);
+            };
+
+            var valueConverters = new ValueConverters()
+                .Add(typeof(Baz), registeredConvertBaz);
+
+            Assert.True(valueConverters.TryGet(typeof(Baz), out var retrievedConvertBaz));
+
+            Assert.Equal(0, invocationCount);
+
+            var converted = retrievedConvertBaz("123.45,-76.543");
+
+            Assert.Equal(1, invocationCount);
+            Assert.IsType<Baz>(converted);
+            Assert.Equal(123.45, ((Baz)converted).Latitude);
+            Assert.Equal(-76.543, ((Baz)converted).Longitude);
+        }
+
+        [Fact]
+        public void TheConvertFuncReturnedByTryGetCallsTheConvertMethodFromTheConvertTypePassedToTheAddByDeclaringTypeAndMemberNameMethod()
+        {
+            var valueConverters = new ValueConverters()
+                .Add(typeof(Foo), "bar", typeof(CountingBarConverter));
+
+            Assert.True(valueConverters.TryGet(typeof(Foo), "bar", out var retrievedConvertBar));
+
+            Assert.Equal(0, CountingBarConverter.InvocationCount);
+
+            var converted = retrievedConvertBar("123");
+
+            Assert.Equal(1, CountingBarConverter.InvocationCount);
+            Assert.IsType<Bar>(converted);
+            Assert.Equal(123, ((Bar)converted).Baz);
+        }
+
+        private static class CountingBarConverter
+        {
+            public static int InvocationCount { get; private set; }
+
+            private static Bar Convert(string value)
+            {
+                InvocationCount++;
+                return new Bar(int.Parse(value));
+            }
+        }
+
+        [Fact]
+        public void TheConvertFuncReturnedByTryGetCallsTheConvertMethodFromTheConvertTypePassedToTheAddByTargetTypeMethod()
+        {
+            var valueConverters = new ValueConverters()
+                .Add(typeof(Baz), typeof(CountingBazConverter));
+
+            Assert.True(valueConverters.TryGet(typeof(Baz), out var retrievedConvertBaz));
+
+            Assert.Equal(0, CountingBazConverter.InvocationCount);
+
+            var converted = retrievedConvertBaz("123.45,-76.543");
+
+            Assert.Equal(1, CountingBazConverter.InvocationCount);
+            Assert.IsType<Baz>(converted);
+            Assert.Equal(123.45, ((Baz)converted).Latitude);
+            Assert.Equal(-76.543, ((Baz)converted).Longitude);
+        }
+
+        private static class CountingBazConverter
+        {
+            public static int InvocationCount { get; private set; }
+
+            private static Baz Convert(string value)
+            {
+                InvocationCount++;
+                return ParseBaz(value);
+            }
+        }
+
+        private static Baz ParseBaz(string value)
+        {
+            var split = value.Split(',');
+            return new Baz(double.Parse(split[0]), double.Parse(split[1]));
+        }
+
+        private static class BarConverter
+        {
+            private static Bar Convert(string value) => new Bar(int.Parse(value));
+        }
+
+        private static class BazConverter
+        {
+            private static Baz Convert(string value) => ParseBaz(value);
+        }
+
+        private class Foo
+        {
+            public Bar Bar { get; set; }
+            public Baz Baz { get; set; }
+        }
+
+        private struct Bar
+        {
+            public Bar(int baz) => Baz = baz;
+            public int Baz { get; }
+        }
+
+        private struct Baz
+        {
+            public Baz(double latitude, double longitude)
+            {
+                Latitude = latitude;
+                Longitude = longitude;
+            }
+            public double Latitude { get; }
+            public double Longitude { get; }
+        }
+    }
+}
---
+++ RockLib.Configuration.ObjectFactory/ValueConverters.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RockLib.Configuration.ObjectFactory
+{
+    /// <summary>
+    /// A container for functions that are used to convert a configuration string value to a
+    /// target type.
+    /// </summary>
+    public class ValueConverters : IValueConverters, IEnumerable<KeyValuePair<string, Type>>
+    {
+        private readonly Dictionary<string, ValueConverter> _converters = new Dictionary<string, ValueConverter>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Configures a converter for the member (property or constructor parameter) specified by the
+        /// provided declaring type and member name. Use this method when you need different members of
+        /// a target type to each use a different converter. If you want all members of a target type to
+        /// use the same converter, use one of the other <see cref="Add(Type, Type)"/> or
+        /// <see cref="Add{T}(Type, Func{string, T})"/> methods.
+        /// </summary>
+        /// <param name="declaringType">The declaring type of a member that needs a converter.</param>
+        /// <param name="memberName">The name of a member that needs a converter.</param>
+        /// <param name="convertFunc">A function that does the conversion from string to <typeparamref name="T"/>.</param>
+        /// <returns>This instance of <see cref="ValueConverters"/>.</returns>
+        /// <typeparam name="T">The concrete type of the object returned by the <paramref name="convertFunc"/> function.</typeparam>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="declaringType"/>, <paramref name="memberName"/>, or <paramref name="convertFunc"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If there are no members of <paramref name="declaringType"/> that match <paramref name="memberName"/>, or
+        /// if the <typeparamref name="T"/> type is not assignable to any of the matching members.
+        /// </exception>
+        public ValueConverters Add<T>(Type declaringType, string memberName, Func<string, T> convertFunc)
+        {
+            if (convertFunc == null) throw new ArgumentNullException(nameof(convertFunc));
+            return Add(declaringType, memberName, typeof(T), value => convertFunc(value));
+        }
+
+        /// <summary>
+        /// Configures a converter for the specified target type. Use this method when you want all
+        /// members (properties or constructor parameters) of the target type to use the same converter.
+        /// If you need different members of a target type to each use a different converter, use one of
+        /// the other <see cref="Add{T}(Type, string, Func{string, T})"/> or
+        /// <see cref="Add(Type, string, Type)"/> methods.
+        /// </summary>
+        /// <param name="targetType">A type that needs a default type.</param>
+        /// <param name="defaultType">The default type for the specified target type.</param>
+        /// <returns>This instance of <see cref="DefaultTypes"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="targetType"/> or <paramref name="convertFunc"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If the <typeparamref name="T"/> type is not assignable to <paramref name="targetType"/>.
+        /// </exception>
+        public ValueConverters Add<T>(Type targetType, Func<string, T> convertFunc)
+        {
+            if (convertFunc == null) throw new ArgumentNullException(nameof(convertFunc));
+            return Add(targetType, typeof(T), value => convertFunc(value));
+        }
+
+        /// <summary>
+        /// Configures a converter for the member (property or constructor parameter) specified by the
+        /// provided declaring type and member name. Use this method when you need different members of
+        /// a target type to each use a different converter. If you want all members of a target type to
+        /// use the same converter, use one of the other <see cref="Add(Type, Type)"/> or
+        /// <see cref="Add{T}(Type, Func{string, T})"/> methods.
+        /// </summary>
+        /// <param name="declaringType">The declaring type of a member that needs a converter.</param>
+        /// <param name="memberName">The name of a member that needs a converter.</param>
+        /// <param name="converterType">
+        /// A type that contains a 'static TReturnType Convert(System.String)' method where TReturnType
+        /// is a type other than System.Object.
+        /// </param>
+        /// <returns>This instance of <see cref="ValueConverters"/>.</returns>
+        /// <typeparam name="T">The concrete type of the object returned by the <paramref name="convertFunc"/> function.</typeparam>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="declaringType"/>, <paramref name="memberName"/>, or <paramref name="convertFunc"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If there are no members of <paramref name="declaringType"/> that match <paramref name="memberName"/>, or
+        /// if the type returned by the converter type's Convert method is not assignable to any of the matching members.
+        /// </exception>
+        public ValueConverters Add(Type declaringType, string memberName, Type converterType)
+        {
+            GetConvertFunc(converterType, out var returnType, out var convertFunc);
+            return Add(declaringType, memberName, returnType, convertFunc);
+        }
+
+        /// <summary>
+        /// Configures a converter for the specified target type. Use this method when you want all
+        /// members (properties or constructor parameters) of the target type to use the same converter.
+        /// If you need different members of a target type to each use a different converter, use one of
+        /// the other <see cref="Add{T}(Type, string, Func{string, T})"/> or
+        /// <see cref="Add(Type, string, Type)"/> methods.
+        /// </summary>
+        /// <param name="targetType">A type that needs a default type.</param>
+        /// <param name="converterType">
+        /// A type that contains a 'static TReturnType Convert(System.String)' method where TReturnType
+        /// is a type other than System.Object.
+        /// </param>
+        /// <returns>This instance of <see cref="DefaultTypes"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="targetType"/> or <paramref name="convertFunc"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If the <typeparamref name="T"/> type is not assignable to <paramref name="targetType"/>.
+        /// </exception>
+        public ValueConverters Add(Type targetType, Type converterType)
+        {
+            GetConvertFunc(converterType, out var returnType, out var convertFunc);
+            return Add(targetType, returnType, convertFunc);
+        }
+
+        /// <summary>
+        /// Attempt to get a convert function for a member (property or construtor parameter) specified by
+        /// its declaring type and name.
+        /// </summary>
+        /// <param name="declaringType">A declaring type of the member to find a converter for.</param>
+        /// <param name="memberName">The name of the member to find a converter for.</param>
+        /// <param name="convertFunc">
+        /// When a match is found for the member, contains the convert function that is used to convert a
+        /// configuration value to the member's type.
+        /// </param>
+        /// <returns>
+        /// True, if a converter was found for the member. Otherwise, false if a converter could not be found.
+        /// </returns>
+        public bool TryGet(Type declaringType, string memberName, out Func<string, object> convertFunc) =>
+            _converters.TryGetValue(GetKey(declaringType, memberName), out ValueConverter converter)
+                ? (convertFunc = converter.ConvertFunc) != null
+                : (convertFunc = null) != null;
+
+        /// <summary>
+        /// Attempt to get a convert function for a specified target type.
+        /// </summary>
+        /// <param name="targetType">The type to find a converter for.</param>
+        /// <param name="convertFunc">
+        /// When a match is found for the member, contains the convert function that is used to convert a
+        /// configuration value to the target type.
+        /// </param>
+        /// <returns>
+        /// True, if a converter was found for the member. Otherwise, false if a converter could not be found.
+        /// </returns>
+        public bool TryGet(Type targetType, out Func<string, object> convertFunc) =>
+            _converters.TryGetValue(GetKey(targetType), out ValueConverter converter)
+                ? (convertFunc = converter.ConvertFunc) != null
+                : (convertFunc = null) != null;
+
+        private ValueConverters Add(Type declaringType, string memberName, Type returnType, Func<string, object> convertFunc)
+        {
+            if (declaringType == null) throw new ArgumentNullException(nameof(declaringType));
+            if (memberName == null) throw new ArgumentNullException(nameof(memberName));
+
+            var matchingMembers = Members.Find(declaringType, memberName).ToList();
+
+            if (matchingMembers.Count == 0) throw new ArgumentException("no matching members");
+            var notAssignableMembers = matchingMembers.Where(m => !m.Type.GetTypeInfo().IsAssignableFrom(returnType)).ToList();
+            if (notAssignableMembers.Count > 0) throw new ArgumentException("return type not assignable to member type");
+
+            _converters.Add(GetKey(declaringType, memberName), new ValueConverter(returnType, convertFunc));
+            return this;
+        }
+
+        private ValueConverters Add(Type targetType, Type returnType, Func<string, object> convertFunc)
+        {
+            if (targetType == null) throw new ArgumentNullException(nameof(targetType));
+            if (!targetType.GetTypeInfo().IsAssignableFrom(returnType)) throw new ArgumentException("must be able to assign returnType to targetType");
+            _converters.Add(GetKey(targetType), new ValueConverter(returnType, convertFunc));
+            return this;
+        }
+
+        private static void GetConvertFunc(Type converterType, out Type returnType, out Func<string, object> convertFunc)
+        {
+            if (converterType == null) throw new ArgumentNullException(nameof(converterType));
+            var convertMethod = converterType.GetTypeInfo().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
+                .SingleOrDefault(m =>
+                    m.Name == "Convert"
+                    && m.GetParameters().Length == 1 && m.GetParameters()[0].ParameterType == typeof(string)
+                    && m.ReturnType != typeof(void) && m.ReturnType != typeof(object));
+            if (convertMethod == null) throw new ArgumentException($"The converter type, {converterType}, must have a 'static TReturnType Convert(string)' method, where TReturnType is a type other than System.Object.");
+            returnType = convertMethod.ReturnType;
+            convertFunc = value => convertMethod.Invoke(null, new object[] { value });
+        }
+
+        private static string GetKey(Type declaringType, string memberName) =>
+                (declaringType != null && memberName != null) ? declaringType.FullName + "::" + memberName : "";
+
+        private static string GetKey(Type targetType) => targetType != null ? targetType.FullName : "";
+
+        private class ValueConverter
+        {
+            public readonly Type ReturnType;
+            public readonly Func<string, object> ConvertFunc;
+
+            public ValueConverter(Type returnType, Func<string, object> convertFunc)
+            {
+                ReturnType = returnType;
+                ConvertFunc = convertFunc;
+            }
+        }
+
+        IEnumerator<KeyValuePair<string, Type>> IEnumerable<KeyValuePair<string, Type>>.GetEnumerator() => Enumerable.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Enumerable).GetEnumerator();
+        private IEnumerable<KeyValuePair<string, Type>> Enumerable =>
+            _converters.Select(x => new KeyValuePair<string, Type>(x.Key, x.Value.ReturnType));
+    }
+}
