ebaa9e3f64e0ac1e78cb065eba8cfcb168c528d4
Friesen <brianfriesen@quickenloans.com>
2017-11-14 14:11:44 -0500

    Add xml doc comments

--- RockLib.Configuration.ProxyFactory/ConfigurationProxyFactory.cs
+++ RockLib.Configuration.ProxyFactory/ConfigurationProxyFactory.cs
@@ -9,6 +9,9 @@ using System.Reflection.Emit;
 
 namespace RockLib.Configuration.ProxyFactory
 {
+    /// <summary>
+    /// Static class that creates proxy instances of interfaces from configuration values.
+    /// </summary>
     public static class ConfigurationProxyFactory
     {
         private const TypeAttributes _typeAttributes = TypeAttributes.NotPublic | TypeAttributes.Class | TypeAttributes.AutoClass | TypeAttributes.AnsiClass | TypeAttributes.BeforeFieldInit | TypeAttributes.AutoLayout;
@@ -16,9 +19,62 @@ namespace RockLib.Configuration.ProxyFactory
 
         private static readonly ConcurrentDictionary<Type, Type> _proxyCache = new ConcurrentDictionary<Type, Type>();
 
+        /// <summary>
+        /// Returns an instance of a proxy type that implements the interface specified by the <typeparamref name="T"/>
+        /// type parameter and has values specified by the <paramref name="configuration"/> parameter.
+        /// </summary>
+        /// <remarks>
+        /// This method uses the <see cref="ConfigurationObjectFactory.Create(IConfiguration, Type, DefaultTypes, ValueConverters)"/>
+        /// method to create instances of proxy types and can throw any of the exception that it throws.
+        /// </remarks>
+        /// <typeparam name="T">An interface with readable properties.</typeparam>
+        /// <param name="configuration">The configuration to create the proxy object from.</param>
+        /// <param name="defaultTypes">
+        /// An object that defines the default types to be used when a type is not explicitly specified by a
+        /// configuration section.
+        /// </param>
+        /// <param name="valueConverters">
+        /// An object that defines custom converter functions that are used to convert string configuration
+        /// values to a target type.
+        /// </param>
+        /// <returns>
+        /// An object that implements the interface specified by the <paramref name="type"/> parameter containing values from the
+        /// <paramref name="configuration"/> parameter.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="configuration"/> is null.</exception>
+        /// <exception cref="ArgumentException">
+        /// If <typeparamref name="T"/> is not an interface or has any declared methods, events, or write-only properties.
+        /// </exception>
         public static T CreateProxy<T>(this IConfiguration configuration, DefaultTypes defaultTypes = null, ValueConverters valueConverters = null) =>
             (T)configuration.CreateProxy(typeof(T), defaultTypes, valueConverters);
 
+        /// <summary>
+        /// Returns an instance of a proxy type that implements the interface specified by the <paramref name="type"/>
+        /// parameter and has values specified by the <paramref name="configuration"/> parameter.
+        /// <para></para>
+        /// </summary>
+        /// <remarks>
+        /// This method uses the <see cref="ConfigurationObjectFactory.Create(IConfiguration, Type, DefaultTypes, ValueConverters)"/>
+        /// method to create instances of proxy types and can throw any of the exception that it throws.
+        /// </remarks>
+        /// <param name="configuration">The configuration to create the proxy object from.</param>
+        /// <param name="type">An interface with readable properties.</param>
+        /// <param name="defaultTypes">
+        /// An object that defines the default types to be used when a type is not explicitly specified by a
+        /// configuration section.
+        /// </param>
+        /// <param name="valueConverters">
+        /// An object that defines custom converter functions that are used to convert string configuration
+        /// values to a target type.
+        /// </param>
+        /// <returns>
+        /// An object that implements the interface specified by the <paramref name="type"/> parameter containing values from the
+        /// <paramref name="configuration"/> parameter.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="configuration"/> or <paramref name="type"/> is null.</exception>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="type"/> is not an interface or has any declared methods, events, or write-only properties.
+        /// </exception>
         public static object CreateProxy(this IConfiguration configuration, Type type, DefaultTypes defaultTypes = null, ValueConverters valueConverters = null)
         {
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
