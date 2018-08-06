557715bfea76f2793c2ff93aa5deceea91a16a61
Brian Friesen <brianfriesen@quickenloans.com>
2018-08-06 13:07:41 -0400

    Expand constructor order tests

--- RockLib.Configuration.ObjectFactory.UnitTests/ConstructorOrderInfoTests.cs
+++ RockLib.Configuration.ObjectFactory.UnitTests/ConstructorOrderInfoTests.cs
@@ -31,12 +31,20 @@ namespace Tests
         [InlineData(typeof(DefaultConstructor), true)]
         [InlineData(typeof(OneParameter), true, "bar")]
         [InlineData(typeof(OneParameter), false)]
+        [InlineData(typeof(OneOptionalParameter), true, "bar")]
+        [InlineData(typeof(OneOptionalParameter), false)]
         [InlineData(typeof(TwoParameters), true, "bar", "baz")]
         [InlineData(typeof(TwoParameters), false, "bar")]
+        [InlineData(typeof(TwoParameters), false, "baz")]
         [InlineData(typeof(TwoParameters), false)]
         [InlineData(typeof(OneParameterOneOptionalParameter), true, "bar", "baz")]
         [InlineData(typeof(OneParameterOneOptionalParameter), false, "bar")]
+        [InlineData(typeof(OneParameterOneOptionalParameter), false, "baz")]
         [InlineData(typeof(OneParameterOneOptionalParameter), false)]
+        [InlineData(typeof(TwoOptionalParameters), true, "bar", "baz")]
+        [InlineData(typeof(TwoOptionalParameters), false, "bar")]
+        [InlineData(typeof(TwoOptionalParameters), false, "baz")]
+        [InlineData(typeof(TwoOptionalParameters), false)]
         public void IsInvokableWithoutDefaultParametersIsCorrect(Type type, bool expectedIsInvokableWithoutDefaultParameters, params string[] resolvableMemberNames)
         {
             var constructor = type.GetTypeInfo().GetConstructors()[0];
@@ -52,12 +60,20 @@ namespace Tests
         [InlineData(typeof(DefaultConstructor), true)]
         [InlineData(typeof(OneParameter), true, "bar")]
         [InlineData(typeof(OneParameter), false)]
+        [InlineData(typeof(OneOptionalParameter), true, "bar")]
+        [InlineData(typeof(OneOptionalParameter), true)]
         [InlineData(typeof(TwoParameters), true, "bar", "baz")]
         [InlineData(typeof(TwoParameters), false, "bar")]
+        [InlineData(typeof(TwoParameters), false, "baz")]
         [InlineData(typeof(TwoParameters), false)]
         [InlineData(typeof(OneParameterOneOptionalParameter), true, "bar", "baz")]
         [InlineData(typeof(OneParameterOneOptionalParameter), true, "bar")]
+        [InlineData(typeof(OneParameterOneOptionalParameter), false, "baz")]
         [InlineData(typeof(OneParameterOneOptionalParameter), false)]
+        [InlineData(typeof(TwoOptionalParameters), true, "bar", "baz")]
+        [InlineData(typeof(TwoOptionalParameters), true, "bar")]
+        [InlineData(typeof(TwoOptionalParameters), true, "baz")]
+        [InlineData(typeof(TwoOptionalParameters), true)]
         public void IsInvokableWithDefaultParametersIsCorrect(Type type, bool expectedIsInvokableWithDefaultParameters, params string[] resolvableMemberNames)
         {
             var constructor = type.GetTypeInfo().GetConstructors()[0];
@@ -97,8 +113,10 @@ namespace Tests
 
         private class DefaultConstructor { }
         private class OneParameter { public OneParameter(int bar) { } }
+        private class OneOptionalParameter { public OneOptionalParameter(int bar = -1) { } }
         private class TwoParameters { public TwoParameters(int bar, int baz) { } }
         private class OneParameterOneOptionalParameter { public OneParameterOneOptionalParameter(int bar, int baz = -1) { } }
+        private class TwoOptionalParameters { public TwoOptionalParameters(int bar = -1, int baz = -1) { } }
         private class ThreeOptionalParameters { public ThreeOptionalParameters(int bar = -1, int baz = -1, int qux = -1) { } }
     }
 }
