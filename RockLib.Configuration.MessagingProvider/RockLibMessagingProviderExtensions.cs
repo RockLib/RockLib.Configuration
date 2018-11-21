4cbd0935c8cdcaba9d534395d1346a8e82a42cd9
Brian Friesen <brian.friesen@gmail.com>
2018-11-21 13:02:34 -0500

    Build the builder in order to create the receiver
    Don't access MessagingScenarioFactory.Configuration unnecessarily.

--- RockLib.Configuration.MessagingProvider/RockLibMessagingProviderExtensions.cs
+++ RockLib.Configuration.MessagingProvider/RockLibMessagingProviderExtensions.cs
@@ -6,7 +6,7 @@ namespace RockLib.Configuration.MessagingProvider
     public static class RockLibMessagingProviderExtensions
     {
         public static IConfigurationBuilder AddRockLibMessagingProvider(this IConfigurationBuilder builder, string scenarioName) =>
-            builder.AddRockLibMessagingProvider(MessagingScenarioFactory.CreateReceiver(scenarioName));
+            builder.AddRockLibMessagingProvider(builder.Build().GetSection("RockLib.Messaging").CreateReceiver(scenarioName));
 
         public static IConfigurationBuilder AddRockLibMessagingProvider(this IConfigurationBuilder builder, IReceiver receiver) =>
             builder.Add(new MessagingConfigurationSource(receiver));
