2261c08974bf80b43860e85d63045c9a83a133bb
Brian Friesen <brianfriesen@quickenloans.com>
2018-12-20 13:06:42 -0500

    Improve usability of Resolver class
    Thanks for the feedback Justin!

--- RockLib.Configuration.ObjectFactory/Resolver.cs
+++ RockLib.Configuration.ObjectFactory/Resolver.cs
@@ -5,27 +5,111 @@ namespace RockLib.Configuration.ObjectFactory
 {
     public class Resolver : IResolver
     {
-        public Resolver(Func<ParameterInfo, bool> canResolve, Func<ParameterInfo, object> resolve)
+        public Resolver(Func<Type, object> resolve)
         {
-            CanResolve = canResolve ?? throw new ArgumentNullException(nameof(canResolve));
-            Resolve = resolve ?? throw new ArgumentNullException(nameof(resolve));
+            if (resolve == null) throw new ArgumentNullException(nameof(resolve));
+
+            CanResolve = p =>
+            {
+                try { return resolve(p.ParameterType) != null; }
+                catch { return false; }
+            };
+
+            Resolve = p =>
+            {
+                try { return resolve(p.ParameterType); }
+                catch { return null; }
+            };
         }
 
-        public static Resolver FromDelegates(Func<Type, bool> canResolve, Func<Type, object> resolve)
+        public Resolver(Func<Type, bool> canResolve, Func<Type, object> resolve)
         {
             if (canResolve == null) throw new ArgumentNullException(nameof(canResolve));
             if (resolve == null) throw new ArgumentNullException(nameof(resolve));
-            return new Resolver(p => canResolve(p.ParameterType), p => resolve(p.ParameterType));
+
+            CanResolve = p =>
+            {
+                try { return canResolve(p.ParameterType); }
+                catch { return false; }
+            };
+
+            Resolve = p =>
+            {
+                try { return resolve(p.ParameterType); }
+                catch { return null; }
+            };
         }
 
-        public static Resolver FromDelegates(Func<Type, string, bool> canResolve, Func<Type, string, object> resolve)
+        public Resolver(Func<Type, object> resolve, Func<Type, string, object> resolveNamed)
+        {
+            if (resolve == null) throw new ArgumentNullException(nameof(resolve));
+            if (resolveNamed == null) throw new ArgumentNullException(nameof(resolveNamed));
+
+            CanResolve = p =>
+            {
+                try
+                {
+                    if (resolveNamed(p.ParameterType, p.Name) != null)
+                        return true;
+                }
+                catch {}
+
+                try { return resolve(p.ParameterType) != null; }
+                catch { return false; }
+            };
+
+            Resolve = p =>
+            {
+                try
+                {
+                    var obj = resolveNamed(p.ParameterType, p.Name);
+                    if (obj != null)
+                        return obj;
+                }
+                catch {}
+
+                try { return resolve(p.ParameterType); }
+                catch { return null; }
+            };
+        }
+
+        public Resolver(Func<Type, bool> canResolve, Func<Type, string, bool> canResolveNamed,
+            Func<Type, object> resolve, Func<Type, string, object> resolveNamed)
         {
             if (canResolve == null) throw new ArgumentNullException(nameof(canResolve));
+            if (canResolveNamed == null) throw new ArgumentNullException(nameof(canResolveNamed));
             if (resolve == null) throw new ArgumentNullException(nameof(resolve));
-            return new Resolver(p => canResolve(p.ParameterType, p.Name), p => resolve(p.ParameterType, p.Name));
+            if (resolveNamed == null) throw new ArgumentNullException(nameof(resolveNamed));
+
+            CanResolve = p =>
+            {
+                try
+                {
+                    if (canResolveNamed(p.ParameterType, p.Name))
+                        return true;
+                }
+                catch {}
+
+                try { return canResolve(p.ParameterType); }
+                catch { return false; }
+            };
+
+            Resolve = p =>
+            {
+                try
+                {
+                    var obj = resolveNamed(p.ParameterType, p.Name);
+                    if (obj != null)
+                        return obj;
+                }
+                catch {}
+
+                try { return resolve(p.ParameterType); }
+                catch { return null; }
+            };
         }
 
-        public static Resolver Empty { get; } = new Resolver(p => false, p => null);
+        public static Resolver Empty { get; } = new Resolver(t => false, t => null);
 
         public Func<ParameterInfo, bool> CanResolve { get; }
 
@@ -37,14 +121,8 @@ namespace RockLib.Configuration.ObjectFactory
         {
             if (CanResolve(parameter))
             {
-                try
-                {
-                    value = Resolve(parameter);
-                    return value != null;
-                }
-                catch
-                {
-                }
+                value = Resolve(parameter);
+                return value != null;
             }
 
             value = null;
