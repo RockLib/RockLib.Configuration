de2dbcbc522e2f71627276c4023cd115b22dde8f
Brian Friesen <brianfriesen@quickenloans.com>
2021-02-11 15:52:34 -0500

    Remove build folder
    Co-authored-by: Justin Moss <justin.moss@gmail.com>

--- build/buildNuget.bat
+++
@@ -1,7 +0,0 @@
-nuget restore -SolutionDirectory ../  ../RockLib.Configuration/RockLib.Configuration.csproj
-
-msbuild /p:Configuration=Release /t:Clean ..\RockLib.Configuration\RockLib.Configuration.csproj
-
-msbuild /p:Configuration=Release /t:Rebuild ..\RockLib.Configuration\RockLib.Configuration.csproj
-
-msbuild /t:pack /p:PackageOutputPath=..\builtPackages  /p:Configuration=Release ..\RockLib.Configuration\RockLib.Configuration.csproj
--- build/buildObjectFactoryNuget.bat
+++
@@ -1,7 +0,0 @@
-nuget restore -SolutionDirectory ../  ../RockLib.Configuration.ObjectFactory/RockLib.Configuration.ObjectFactory.csproj
-
-msbuild /p:Configuration=Release /t:Clean ..\RockLib.Configuration.ObjectFactory\RockLib.Configuration.ObjectFactory.csproj
-
-msbuild /p:Configuration=Release /t:Rebuild ..\RockLib.Configuration.ObjectFactory\RockLib.Configuration.ObjectFactory.csproj
-
-msbuild /t:pack /p:PackageOutputPath=..\builtPackages  /p:Configuration=Release ..\RockLib.Configuration.ObjectFactory\RockLib.Configuration.ObjectFactory.csproj
