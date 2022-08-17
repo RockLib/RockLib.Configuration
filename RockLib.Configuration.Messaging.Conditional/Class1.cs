38919ac5a7d6a2736ca4f2232d5f459b6bdb903e
Jason Bock <jason.r.bock@outlook.com>
2022-08-17 08:52:50 -0500

    Initial assets/project files/folders/etc. setup (#86)
    * #437 initial assets/project files/folders/etc. setup
    
    * #447 changing the .sln file version back
    
    Co-authored-by: Jason Bock <jasonbock1@rocketmortgage.com>

---
+++ RockLib.Configuration.Messaging.Conditional/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace RockLib.Configuration.Messaging.Conditional;
+
+/// <summary>
+/// Class1
+/// </summary>
+public static class Class1
+{
+    // TODO: Remove once real code is added (just a placeholder to CA1014, even though that is turned on in .editorconfig)
+}
