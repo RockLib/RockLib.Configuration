a9a2b7dbb0b1ef7e86d19af104c7a4a6f75fad58
Tom McDonald <tommcdonald@rocketcentral.com>
2022-09-22 14:50:28 -0400

    Add shell for RockLib.Configuration.Remote

---
+++ Tests/RockLib.Configuration.Remote.Tests/Class1Tests.cs
@@ -0,0 +1,9 @@
+﻿namespace RockLib.Configuration.Remote.Tests;
+
+/// <summary>
+/// Class1Tests
+/// </summary>
+public static class Class1Tests
+{
+    // TODO: Remove once real code is added (just a placeholder to CA1014, even though that is turned on in .editorconfig)
+}
